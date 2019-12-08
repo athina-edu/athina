dceedfbca5a27bc55e814db0d7e52eae55b130cd
Michael <tsikerdekis@gmail.com>
2019-12-07 17:32:33 -0800

    Pipenv support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='athina',
-    version='0.95',
+    version='0.97',
     packages=find_packages(),
     scripts=['bin/athina-cli'],
     install_requires=['np', 'filelock', 'python-dateutil', 'requests', 'numpy', 'peewee', 'mosspy', 'pyyaml',
