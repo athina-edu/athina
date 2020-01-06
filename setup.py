b6067da9d69c834ab0a837ed9fd537056e9f971c
tsikerdekis <tsikerdekis@gmail.com>
2020-01-06 12:53:17 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(),
     scripts=['bin/athina-cli'],
     install_requires=['np', 'filelock', 'python-dateutil', 'requests', 'numpy', 'peewee', 'mosspy', 'pyyaml',
-                      'psutil', 'gitpython', 'pymysql'],
+                      'psutil', 'gitpython', 'pymysql', 'requests'],
     url='https://github.com/athina-edu/athina',
     license='MIT',
     author='Michail Tsikerdekis',
